f4fcdfe2c5e50be878db8e1b3e90a6cc270ff26c
terenty.rezman <terenty.rezman@mail.ru>
2021-01-21 16:47:39 +0300

    done with assign 09, not quite correct but will do

--- assignment_09/mymounts.c
+++ assignment_09/mymounts.c
@@ -1,3 +1,9 @@
+/*
+ * this module works only in kernel versions before this patch 
+ * https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?h=v4.19.23&id=0bd476e6c6
+ * (below v4.19.23)
+ */
+
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
 #include <linux/kernel.h>
@@ -14,14 +20,27 @@ static struct vfsmount *(*kallsym_collect_mounts)(struct path *);
 static void (*kallsym_drop_collected_mounts)(struct vfsmount *);
 static int (*kallsym_iterate_mounts)(int (*)(struct vfsmount *, void *), void *, struct vfsmount *);
 
+/*
+ * taken from /fs/proc_namespace.c 
+ * show_mountinfo()
+ * https://elixir.bootlin.com/linux/v4.9.252/source/fs/proc_namespace.c#L128
+ * 
+ */
 static int print_mount(struct vfsmount *mnt, void *arg)
 {
 	struct seq_file *m = arg;
-	struct dentry *dentry;
+	struct path path = { .dentry = mnt->mnt_root, .mnt = mnt };
+	struct super_block *sb = path.dentry->d_sb;
 
-	dentry = mnt->mnt_root;
+	if (sb->s_op->show_devname)
+		sb->s_op->show_devname(m, path.dentry);
+	else
+		seq_puts(m, sb->s_id);
 
-	seq_printf(m, "%s\n", dentry->d_name.name);
+	seq_putc(m, '\t');
+	seq_putc(m, '\t');
+	seq_path(m, &path, "");
+	seq_putc(m, '\n');
 	return 0;
 }
 
@@ -43,7 +62,6 @@ static int mymounts_show(struct seq_file *m, void *v)
 
 	kallsym_drop_collected_mounts(root_mnt);
 	path_put(&root_path);
-
 	return 0;
 }
 
@@ -67,7 +85,7 @@ static int __init mymounts_init(void)
 
 	entry = proc_create("mymounts", S_IRUSR, NULL, &mymounts_fops);
 	if (!entry) {
-		pr_err("Failed to create error_log proc entry\n");
+		pr_err("mymounts: Failed to create error_log proc entry\n");
 		return -ENOMEM;
 	}
 
@@ -82,14 +100,14 @@ static int __init mymounts_init(void)
 		return 1;
 	}
 
-	pr_info("ft loaded\n");
+	pr_info("mymounts loaded\n");
 	return 0;
 }
 
 static void __exit mymounts_exit(void)
 {
 	remove_proc_entry("mymounts", NULL);
-	pr_info("ft unloaded\n");
+	pr_info("mymounts unloaded\n");
 }
 
 module_init(mymounts_init);
